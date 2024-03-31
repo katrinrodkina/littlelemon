2140ef22a72ce004f48e0ab2bca90c0b8f3274ab
Ekaterina <erodkina@workfusion.com>
2024-03-31 19:08:17 -0400

    Initial Commit

---
+++ littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by katrina on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
