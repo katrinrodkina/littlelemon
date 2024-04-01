c9cf37b80695cad49e5c93b81bf3c91ba1529791
Ekaterina <erodkina@workfusion.com>
2024-04-01 13:35:28 -0400

    setting up navigation

---
+++ littlelemon/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  littlelemon
+//
+//  Created by katrina on 4/1/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
